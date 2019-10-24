1033ac56fabffcf62962a0558530245fddd59cb3
Sivert Sømme Sagmo <au549312@uni.au.dk>
2019-10-24 20:00:59 +0200

    e2

---
+++ testProg/ATM_1/ATM_1/Decoder/IDecoder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_1
+{
+    public interface IDecoder
+    {
+    	void DecodeData(RawTransponderDataEventArgs e);
+    }
+}
