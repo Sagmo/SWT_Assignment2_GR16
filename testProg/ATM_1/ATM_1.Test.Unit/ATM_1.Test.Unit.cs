9832310be8268383e59252a4d3839310f38ffe9f
Glenn Laursen <201703930@post.au.dk>
2019-10-31 13:39:22 +0100

    2

--- testProg/ATM_1/ATM_1.Test.Unit/ATM_1.Test.Unit.cs
+++ testProg/ATM_1/ATM_1.Test.Unit/ATM_1.Test.Unit.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using ATM_1;
 using NUnit.Framework;
+using TransponderReceiver;
+
+
 namespace ATM_1.Test.Unit
 {
     [TestFixture]
     public class ATMTest
     {
-     
+        
     }
 }
