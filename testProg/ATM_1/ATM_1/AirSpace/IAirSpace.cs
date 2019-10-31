dacf8fa7e9f5ef00766c4a4f9fd1c6bd7afe3af9
Saeed Soltani <au583047@uni.au.dk>
2019-10-31 17:21:45 +0100

    ændret .netcore framworket til 2.2 i stedet for 3.0 da jenkins ikke understøtter 3.0

--- testProg/ATM_1/ATM_1/AirSpace/IAirSpace.cs
+++ testProg/ATM_1/ATM_1/AirSpace/IAirSpace.cs
@@ -5,9 +5,9 @@ namespace ATM_1
     public interface IAirSpace
     {
         //void AirSpace(double x, double y, double lowerBoundary, double upperBoundary); 
-        public double _X             { set; get; }
-        public double _Y             { set; get; }
-        public double _LowerBoundary { set; get; }
-        public double _UpperBoundary { set; get; }
+        double _X             { set; get; }
+        double _Y             { set; get; }
+        double _LowerBoundary { set; get; }
+        double _UpperBoundary { set; get; }
     }
 }
