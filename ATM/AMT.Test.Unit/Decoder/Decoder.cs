7e10d9c0bd0c5361cce536484c39925410193fbc
sagmo <sivert.sag@gmail.com>
2019-11-01 12:14:05 +0100

    atm decoder test started

---
+++ ATM/AMT.Test.Unit/Decoder/Decoder.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using ATM;
+using NSubstitute;
+using TransponderReceiver;
+
+namespace ATM.Test.Unit
+{
+    [TestFixture]
+    public class DecoderTest
+    {
+        private Decoder _uut;
+        private IObjstruct _flightObj;
+    }
+}
