300eeaef7f2e4ec6f228f6bcaa1c348168126ae7
Glenn Laursen <201703930@post.au.dk>
2019-11-01 21:50:29 +0100

    ObjectStructureTests maybe done

--- AMT.Test.Unit/ObjectStructureTest/ObjectStructureTest.cs
+++ AMT.Test.Unit/ObjectStructureTest/ObjectStructureTest.cs
@@ -12,25 +12,58 @@ namespace AMT.Test.Unit
     [TestFixture]
    	public class ObjectStructureTest
     {
-        private FlightObject _uut;
+        private IObjStruct _uut;
         private IVali _validator;
-        private ITrack _track;
-        private bool _event;
+        //private ITrack _track;
+        //private bool _event;
 
         [SetUp]
         public void Setup()
         {
             _validator = Substitute.For<IVali>();
-
-        }
-
-        /*
-        [Test]
-        public void Test_mer()
-        {
+            _validator.Validate(Arg.Any<ITrack>()).Returns(true);
             _uut = new FlightObject(_validator);
-            _uut.Attach();
         }
-        */
+
+        [Test] 
+        public void SuccessfullyAttachTrack()
+        {
+            ITrack track = new Track("thg", "6000", "5800", "10000", "20151006213456789");
+            _uut.Attach(track);
+            Assert.That(_uut.getlist()[0],Is.EqualTo(track));
+        }
+
+        [Test]
+        public void SuccessfullyDetachTrack()
+        {
+            ITrack track1 = new Track("thg", "6000", "5800", "10000", "20151006213456789");
+            ITrack track2 = new Track("th3", "6000", "5800", "10000", "20151006213456789");
+            _uut.Attach(track1);
+            _uut.Attach(track2);
+            _uut.Detach(track2);
+            Assert.That(_uut.getlist(),Does.Not.Contain(track2));
+            Assert.That(_uut.getlist(), Does.Contain(track1));
+        }
+
+        [Test]
+        public void SuccessfullyGetList()
+        {
+            ITrack track1 = new Track("thg", "6000", "5800", "10000", "20151006213456789");
+            ITrack track2 = new Track("th3", "6000", "5800", "10000", "20151006213456789");
+            List<ITrack> list = new List<ITrack>() {track1, track2};
+            _uut.Attach(track1);
+            _uut.Attach(track2);
+            Assert.That(_uut.getlist(), Is.EquivalentTo(list));
+        }
+
+        [Test]
+        public void CheckExistTrue()
+        {
+            ITrack track1 = new Track("thg", "6000", "5800", "10000", "20151006213456789");
+            ITrack track2 = new Track("th3", "6000", "5800", "10000", "20151006213456789");
+            _uut.Attach(track1);
+            Assert.That(_uut.CheckExist(track1.Tag),Is.True);
+            Assert.That(_uut.CheckExist(track2.Tag),Is.False);
+        }
     }
 }
