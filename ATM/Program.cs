09877179c6325f0e72a39ad38c41bed23027e0cf
Glenn Laursen <201703930@post.au.dk>
2019-11-02 00:01:26 +0100

    Goodnight

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -17,13 +17,10 @@ namespace ATM_1
             IWriter _writer = new Writer("log.txt");
             ISeperation _seperation = new Seperation(_decoder);
             Log _log = new Log(_seperation, _writer, _decoder);
-            //ITrack track1 = new Track("1","2","3","4","5");
-            //ITrack track2 = new Track("6", "7", "8", "9", "10");
-            //List<ITrack> list = new List<ITrack>(){track2,track1};
+            
             while (true)
             {
-                //Thread.Sleep(500);
-                //_writer.WriteFile(list);
+                
             }
         }
     }
