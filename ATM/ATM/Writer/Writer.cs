0a6517f4d1979935f1f1cb3eaa674d98a611a8d8
Glenn Laursen <201703930@post.au.dk>
2019-11-01 13:21:55 +0100

    Writer

--- ATM/ATM/Writer/Writer.cs
+++ ATM/ATM/Writer/Writer.cs
@@ -16,23 +16,16 @@ namespace ATM_1
         {
             _sw = File.CreateText(path);
             _sw.Close();
+            _sw = new StreamWriter(path);
         }
         public void WriteFile(List<ITrack> list)
         {
             string path = "log.txt";
             const string dateFormat = "yyyyMMddHHmmssfff";
             CultureInfo cultureInfo = new CultureInfo("da-DK");
-            
-            _sw = new StreamWriter(path);
-            foreach (ITrack track in list)
-            {
-                var timestamp = DateTime.ParseExact(track.TimeStamp, dateFormat, cultureInfo);
-                _sw.WriteLineAsync($"{track.Tag}\t" +
-                             $"[{track.xCoordinate}:{track.yCoordinate}]\t" +
-                             $"[{track.Altitude}]\t" +
-                             $"[{timestamp}]\t");
-            }
-            _sw.Close();
+            var timestamp = DateTime.ParseExact(list[0].TimeStamp, dateFormat, cultureInfo);
+            _sw.WriteLine($"[{list[0].Tag}]\t" + $"[{list[1].Tag}]\t" + $"[{timestamp}]\t");
+            _sw.Flush();
         }
 
         public void WriteConsole(List<ITrack> list)
@@ -44,7 +37,7 @@ namespace ATM_1
             foreach (ITrack track in list)
             {
                 var timestamp = DateTime.ParseExact(track.TimeStamp, dateFormat, cultureInfo);
-                Console.WriteLine($"{track.Tag}\t" +
+                Console.WriteLine($"[{track.Tag}]\t" +
                                   $"[{track.xCoordinate}:{track.yCoordinate}]\t" +
                                   $"[{track.Altitude}]\t" +
                                   $"[{timestamp}]\t");
@@ -57,7 +50,7 @@ namespace ATM_1
             CultureInfo cultureInfo = new CultureInfo("da-DK");
             var timestamp1 = DateTime.ParseExact(list[0].TimeStamp, dateFormat, cultureInfo);
             var timestamp2 = DateTime.ParseExact(list[1].TimeStamp, dateFormat, cultureInfo);
-
+            Console.Clear();
             Console.WriteLine("Seperation: Tag1: {0}, Tag2: {1}, Timestamp1: {2}, Timestamp2: {3}", list[0].Tag, list[1].Tag,
                 timestamp1, timestamp2);
         }
