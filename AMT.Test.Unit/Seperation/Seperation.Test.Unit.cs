e3973a563231ee88cc9423bc21728292d1cc3f46
Glenn Laursen <201703930@post.au.dk>
2019-11-01 21:23:56 +0100

    Seperation test are now good

--- AMT.Test.Unit/Seperation/Seperation.Test.Unit.cs
+++ AMT.Test.Unit/Seperation/Seperation.Test.Unit.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ATM_1;
 using NSubstitute;
+using NSubstitute.Extensions;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
@@ -17,40 +18,35 @@ namespace AMT.Test.Unit.Seperation
         private DecoderEventArgs _decoderEventArgs;
         private SeperationWarningEventArgs _seperationWarningEventArgs;
         private IObjStruct _opj;
-        private IVali _vali;
-        private IAirSpace _airSpace;
+        private bool _eventHandled = false;
 
-        private bool eventHandled = false;
-        private bool eventRaised = false;
         [SetUp]
         public void Setup()
         {
             _seperationWarningEventArgs = null;
-            //_vali = Substitute.For<IVali>();
-            _airSpace = new AirSpace(80000,80000,500,20000);
-            _vali = new Vali(_airSpace);
             _decoder = Substitute.For<IDecoder>();
-            _opj = Substitute.For<IObjStruct>();
+            _opj = new FakeFlightObj();
             _uut = new ATM_1.Seperation(_decoder);
-            _decoder.DecodeEvent += (sender, args) => eventRaised = true;
+            _decoder.DecodeEvent += (sender, args) => _eventHandled = true;
             _decoder.DecodeEvent += (sender, args) => _decoderEventArgs = args;
             _uut.SeperationWarningEvent += (sender, args) => { _seperationWarningEventArgs = args; };
         }
 
         [Test]
-        public void testtest()
-        {
-            _opj = new FlightObject(_vali);
-            _opj.Attach(new Track("1","2","3","600","5"));
-            _opj.Attach(new Track("1", "2", "3", "600", "5"));
+        public void DecodeEvent_FlightIsAdded_EventIsHandled()
+        { ;
+            ITrack track1 = new Track("1", "2", "3", "600", "5");
+            ITrack track2 = new Track("1", "2", "3", "600", "5");
+            _opj.Attach(track1);
+            _opj.Attach(track2);
+
             _decoder.DecodeEvent += Raise.EventWith(new DecoderEventArgs{FlightObjectStruct = _opj});
-            Assert.That(eventRaised, Is.True);
+            Assert.That(_eventHandled, Is.True);
         }
 
         [Test]
-        public void hej()
+        public void HandleDecodeEvent_FlightsReceived_ListOfFlightReceived()
         {
-            _opj = new FlightObject(_vali);
             _opj.Attach(new Track("1", "2", "3", "600", "5"));
             _opj.Attach(new Track("1", "2", "3", "600", "5"));
             _decoder.DecodeEvent += Raise.EventWith(new DecoderEventArgs { FlightObjectStruct = _opj });
@@ -58,9 +54,8 @@ namespace AMT.Test.Unit.Seperation
         }
 
         [Test]
-        public void hej2()
+        public void CheckForSeparation_FlightAreInSeparation_EventIsRaised()
         {
-            _opj = new FlightObject(_vali);
             _opj.Attach(new Track("thg", "6000", "5800", "10000", "20151006213456789"));
             _opj.Attach(new Track("abc", "6000", "5800", "10000", "20151006213456781"));
 
@@ -69,9 +64,8 @@ namespace AMT.Test.Unit.Seperation
         }
 
         [Test]
-        public void hej3()
+        public void CheckForSeparation_FlightAreInSeparation_ListsEquivalent()
         {
-            _opj = new FlightObject(_vali);
             _opj.Attach(new Track("thg", "6000", "5800", "10000", "20151006213456789"));
             _opj.Attach(new Track("abc", "6000", "5800", "10000", "20151006213456781"));
 
