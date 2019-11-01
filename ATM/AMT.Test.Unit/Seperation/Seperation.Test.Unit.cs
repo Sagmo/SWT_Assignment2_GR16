6b28a18fed674a6aeaf7046404425eebb2b8349f
Glenn Laursen <201703930@post.au.dk>
2019-11-01 14:57:02 +0100

    h

---
+++ ATM/AMT.Test.Unit/Seperation/Seperation.Test.Unit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ATM_1;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AMT.Test.Unit.Seperation
+{
+    [TestFixture]
+    public class SeperationTest
+    {
+        private ISeperation _uut;
+        private IDecoder _decoder;
+        private DecoderEventArgs _decoderEventArgs;
+        private IObjStruct _opj;
+
+        private bool eventHandled = false;
+        private bool eventRaised = false;
+        [SetUp]
+        public void Setup()
+        {
+            _opj = Substitute.For<IObjStruct>();
+            _decoder = Substitute.For<IDecoder>();
+            _uut = new ATM_1.Seperation(_decoder);
+            _decoder.DecodeEvent += (sender, args) => eventRaised = true;
+            _decoder.DecodeEvent += (sender, args) => _decoderEventArgs = args;
+            _uut.SeperationWarningEvent += (sender, args) => eventRaised = true;
+        }
+
+        [Test]
+        public void testtest()
+        {
+            //_opj.Attach(new Track("1", "2", "3", "4", "5"));
+            //_opj.Attach(new Track("1", "2", "3", "4", "5"));
+            _decoder.DecodeEvent += Raise.EventWith(new DecoderEventArgs() {FlightObjectStruct = _opj});
+            Assert.That(eventRaised, Is.True);
+        }
+
+
+    }
+}
