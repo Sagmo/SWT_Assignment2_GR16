2196855b064090a77fcca02b669fcf89c956fec9
Glenn Laursen <201703930@post.au.dk>
2019-11-01 22:46:07 +0100

    Airspace

---
+++ AMT.Test.Unit/Airspace/Airspace.Unit.Test.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ATM_1;
+using NUnit.Framework;
+
+
+namespace AMT.Test.Unit.Airspace
+{
+    [TestFixture]
+    public class AirspaceTest
+    {
+        private IAirSpace _airSpace;
+        [SetUp]
+        public void Setup()
+        {
+            _airSpace = new AirSpace(80000,80000,500,20000);
+        }
+
+        [Test]
+        public void TestAirspaceGetX()
+        {
+            var x = _airSpace._X;
+            Assert.That(_airSpace._X,Is.EqualTo(x));
+        }
+
+        [Test]
+        public void TestAirspaceGetY()
+        {
+            var y = _airSpace._Y;
+            Assert.That(_airSpace._Y,Is.EqualTo(y));
+        }
+
+        [Test]
+        public void TestAirspaceGetLowerBoundary()
+        {
+            var lowerboundary = _airSpace._LowerBoundary;
+            Assert.That(_airSpace._LowerBoundary, Is.EqualTo(lowerboundary));
+        }
+
+        [Test]
+        public void TestAirspaceGetUpperBoundary()
+        {
+            var Upperboundary = _airSpace._UpperBoundary;
+            Assert.That(_airSpace._UpperBoundary, Is.EqualTo(Upperboundary));
+        }
+    }
+}
