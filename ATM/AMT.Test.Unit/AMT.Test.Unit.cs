353af884e6cc9a940d44a813e943b6867e5e3351
Saeed Soltani <au583047@uni.au.dk>
2019-11-01 11:53:25 +0100

    tilføjet test for at få jenkins til at køre

---
+++ ATM/AMT.Test.Unit/AMT.Test.Unit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ATM_1;
+using TransponderReceiver;
+
+namespace AMT.Test.Unit
+{
+    [TestFixture]
+    public class DecoderTest
+    {
+        private IDecoder uut;
+        [SetUp]
+        public void Setup()
+        {
+            IObjStruct fakeObjStruct = Substitute.For<IObjStruct>();
+            ITransponderReceiver fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            uut = new Decoder(fakeObjStruct, fakeTransponderReceiver);
+
+        }
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
