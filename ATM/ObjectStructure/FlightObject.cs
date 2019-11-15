89c6365b3b030fcdbab43b35cffa05fe77fc627c
Sivert Sømme Sagmo <au549312@uni.au.dk>
2019-11-15 12:02:17 +0100

    speed and compass first update

--- ATM/ObjectStructure/FlightObject.cs
+++ ATM/ObjectStructure/FlightObject.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace ATM_1
@@ -21,7 +22,8 @@ namespace ATM_1
             {
                 if(CheckExist(flightTrack.Tag))
                 {
-                    _objects.RemoveAll(item => item.Tag == flightTrack.Tag);
+                    //_objects.RemoveAll(item => item.Tag == flightTrack.Tag);
+                    flightTrack = CalculateSpeed(flightTrack);
                 }
                 
                 _objects.Add(flightTrack);
@@ -34,25 +36,44 @@ namespace ATM_1
             */
         }
 
-        public ITrack CalculateSpeed(ITrack track)
+        public ITrack CalculateSpeed(ITrack newTrack)
         {
-            var flightTrack = _objects.FirstOrDefault(o => o.Tag == track.Tag);
+            var oldTrack = _objects.FirstOrDefault(o => o.Tag == newTrack.Tag);
             
             //if (flightTrack == null) return track;
 
             var posChange = new
             {
-                xSpeed = track.xCoordinate - flightTrack.xCoordinate,
-                ySpeed = track.yCoordinate - flightTrack.yCoordinate
+                xChange = newTrack.xCoordinate - oldTrack.xCoordinate,
+                yChange = newTrack.yCoordinate - oldTrack.yCoordinate
             };
 
+            
             var timeChange = new
             {
-                time1 = track.TimeStamp - flightTrack.TimeStamp,
-                time2 =
+                timeOld = DateTime.ParseExact(oldTrack.TimeStamp, "yyyymmddhhmmssfff", new CultureInfo("dk-DK")),
+                timeNew = DateTime.ParseExact(newTrack.TimeStamp, "yyyymmddhhmmssfff", new CultureInfo("dk-DK"))
             };
 
-            return flightTrack;
+            var time = (timeChange.timeNew - timeChange.timeOld).TotalSeconds;
+            
+            
+            var distance = Math.Sqrt(Math.Pow(posChange.xChange, 2) + Math.Pow(posChange.yChange, 2));
+            double CompassDegree = ((Math.Atan2(posChange.yChange, posChange.xChange) * (180 / Math.PI)) - 90);
+            
+            //(CompassDegree < 0) ? CompassDegree = CompassDegree + 360 : CompassDegree;
+            if (CompassDegree < 0)
+                CompassDegree += 360;
+
+            double Velocity;
+            (time == 0.00) ? throw new DivideByZeroException("Time is zero") : ( (time < 0) ? throw new Exception("Time is negative") : Velocity = distance / time) );
+
+            newTrack.HorizontalVelocity = Velocity;
+            newTrack.CompassCourse = CompassDegree;
+
+            _objects.RemoveAll(item => item.Tag == oldTrack.Tag);
+
+            return newTrack;
         }
 
         public void clearList()
