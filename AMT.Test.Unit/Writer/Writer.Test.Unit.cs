f3bed58fc16be483670802fbbfff2bb20abc4fef
Laursen <201703930@post.au.dk>
2019-11-16 20:52:06 +0100

    tests for writes, need to try on leeroy

--- AMT.Test.Unit/Writer/Writer.Test.Unit.cs
+++ AMT.Test.Unit/Writer/Writer.Test.Unit.cs
@@ -1,21 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ATM_1;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 namespace AMT.Test.Unit.Writer
 {
     [TestFixture]
     public class WriterTest
     {
-        private ATM_1.ILog _ilog;
-        private ATM_1.IWriter _uut;
+        private IWriter _uut;
 
         [SetUp]
         public void Setup()
@@ -30,18 +24,34 @@ namespace AMT.Test.Unit.Writer
         }
 
         [Test]
-        public void test_writeConsole()
+        public void writeConsole_ArgumentIsNull_ThrowsException()
         {
-            Assert.That(() => _uut.WriteConsole(null), Throws.Exception);
+            Assert.That(() => _uut.WriteConsole(null), Throws.TypeOf<NullReferenceException>());
         }
 
         [Test]
-        public void tetst()
+        public void writeConsole_ArgumentIsValidList_DoesNotThrow()
         {
-            List<ITrack> list = new List<ITrack> { new ATM_1.Track("thg", "6000", "5800", "10000", "20151006213456789") };
+            List<ITrack> list = new List<ITrack> { new ATM_1.Track("thg", "6000", "5800", "10000", "20151006213456789"), 
+                new ATM_1.Track("ggg", "6000", "5800", "10000", "20151006213456789") };
+
             Assert.DoesNotThrow(() => _uut.WriteConsole(list));
         }
 
+
+        [Test]
+        public void writeConsole_ArgumentIsEmptyList_ThrowException()
+        {
+            List<ITrack> list = new List<ITrack>();
+
+            Assert.That(() => _uut.WriteConsole(list), Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void writeFile_ArgumentIsNull_ThrowsException()
+        {
+            Assert.That(() => _uut.WriteFile(null), Throws.TypeOf<NullReferenceException>());
+        }
 
     }
 }
--- ATM/Writer/Writer.cs
+++ ATM/Writer/Writer.cs
@@ -18,33 +18,55 @@ namespace ATM_1
         }
         public void WriteFile(List<ITrack> list)
         {
-            
-            var timestamp1 = DateTime.ParseExact(list[0].TimeStamp, DateFormat, _cultureInfo);
-            var timestamp2 = DateTime.ParseExact(list[1].TimeStamp, DateFormat, _cultureInfo);
-            _sw.WriteLine($"Seperation: [{list[0].Tag} & {list[1].Tag}], [Timestamp1: {timestamp1}, Timestamp2: {timestamp2}]");
-            _sw.Flush();
+            if (list.Count > 0)
+            {
+                var timestamp1 = DateTime.ParseExact(list[0].TimeStamp, DateFormat, _cultureInfo);
+                var timestamp2 = DateTime.ParseExact(list[1].TimeStamp, DateFormat, _cultureInfo);
+                _sw.WriteLine(
+                    $"Seperation: [{list[0].Tag} & {list[1].Tag}], [Timestamp1: {timestamp1}, Timestamp2: {timestamp2}]");
+                _sw.Flush();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(list));
+            }
         }
 
         public void WriteConsole(List<ITrack> list)
         {
-            Console.Clear();
-            foreach (ITrack track in list)
+            if (!Console.IsOutputRedirected) Console.Clear();
+            if (list.Count > 0)
             {
-                var timestamp = DateTime.ParseExact(track.TimeStamp, DateFormat, _cultureInfo);
-                Console.WriteLine($"[{track.Tag}]\t" +
-                                  $"[{track.xCoordinate}:{track.yCoordinate}]\t" +
-                                  $"[{track.Altitude} meters]\t" +
-                                  $"[{track.CompassCourse} deg]\t" +
-                                  $"[{track.HorizontalVelocity} m/s]\t" +
-                                  $"[{timestamp}]\t");
+                foreach (ITrack track in list)
+                {
+                    var timestamp = DateTime.ParseExact(track.TimeStamp, DateFormat, _cultureInfo);
+                    Console.WriteLine($"[{track.Tag}]\t" +
+                                      $"[{track.xCoordinate}:{track.yCoordinate}]\t" +
+                                      $"[{track.Altitude} meters]\t" +
+                                      $"[{track.CompassCourse} deg]\t" +
+                                      $"[{track.HorizontalVelocity} m/s]\t" +
+                                      $"[{timestamp}]\t");
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(list));
             }
         }
 
         public void WriteSeperationConsole(List<ITrack> list)
         {
-            var timestamp1 = DateTime.ParseExact(list[0].TimeStamp, DateFormat, _cultureInfo);
-            var timestamp2 = DateTime.ParseExact(list[1].TimeStamp, DateFormat, _cultureInfo);
-            Console.WriteLine($"Seperation: [{list[0].Tag} & {list[1].Tag}], [Timestamp1: {timestamp1}, Timestamp2: {timestamp2}]");
+            if (list.Count > 0)
+            {
+                var timestamp1 = DateTime.ParseExact(list[0].TimeStamp, DateFormat, _cultureInfo);
+                var timestamp2 = DateTime.ParseExact(list[1].TimeStamp, DateFormat, _cultureInfo);
+                Console.WriteLine(
+                    $"Seperation: [{list[0].Tag} & {list[1].Tag}], [Timestamp1: {timestamp1}, Timestamp2: {timestamp2}]");
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(list));
+            }
         }
 
     }
