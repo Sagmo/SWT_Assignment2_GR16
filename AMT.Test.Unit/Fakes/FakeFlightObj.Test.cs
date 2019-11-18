6e63504ac9bcf713071b5f7910d4f6c64d9396c6
Laursen <201703930@post.au.dk>
2019-11-18 10:48:20 +0100

    Useless tests for fake

---
+++ AMT.Test.Unit/Fakes/FakeFlightObj.Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using ATM_1;
+
+namespace AMT.Test.Unit.Fakes
+{
+    [TestFixture]
+
+    class FakeFlightObjTest
+    {
+        private IObjStruct _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new FakeFlightObj();
+        }
+
+        [Test]
+        public void CalculateSpeed_NotImplemented_ExceptionIsThrown()
+        {
+            ITrack track = new ATM_1.Track("a", "1", "2", "3", "e");
+            Assert.That(()=> _uut.CalculateSpeed(track), Throws.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void CheckExist_NotImplemented_ExceptionIsThrown()
+        {
+            Assert.That(() => _uut.CheckExist("Test"), Throws.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void ClearList_NotImplemented_ExceptionIsThrown()
+        {
+            Assert.That(()=> _uut.clearList(), Throws.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void Detach_NotImplemented_ExceptionIsThrown()
+        {
+            ITrack track = new ATM_1.Track("a","1","2","3","e");
+            Assert.That(() => _uut.Detach(track), Throws.TypeOf<NotImplementedException>());
+        }
+    }
+}
--- AMT.Test.Unit/Fakes/FakeFlightObj.cs
+++ AMT.Test.Unit/Fakes/FakeFlightObj.cs
@@ -3,6 +3,7 @@ using ATM_1;
 
 namespace AMT.Test.Unit.Fakes
 {
+    
     public class FakeFlightObj : IObjStruct
     {
         private List<ITrack> _objects;
@@ -17,28 +18,30 @@ namespace AMT.Test.Unit.Fakes
             _objects.Add(track);
         }
 
-        public void Detach(ITrack track) { }
-
-        public bool CheckExist(string newTag)
-        {
-            foreach (ITrack e in _objects)
-            {
-                if (e.Tag == newTag)
-                    return true;
-            }
-            return false;
-        }
+        
         public List<ITrack> getlist()
         {
             return _objects;
         }
 
-        public void clearList() { }
+        public void Detach(ITrack flightTrack)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckExist(string newTag)
+        {
+            throw new System.NotImplementedException();
+        }
 
         public ITrack CalculateSpeed(ITrack newTrack)
         {
-            return null;
+            throw new System.NotImplementedException();
         }
 
+        public void clearList()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
