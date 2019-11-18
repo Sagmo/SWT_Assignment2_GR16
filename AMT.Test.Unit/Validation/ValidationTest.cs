cae7b915406026794af8a375248ff118e326adaf
Glenn Laursen <201703930@post.au.dk>
2019-11-18 13:25:38 +0100

    FIXED STUFF

--- AMT.Test.Unit/Validation/ValidationTest.cs
+++ AMT.Test.Unit/Validation/ValidationTest.cs
@@ -28,11 +28,11 @@ namespace AMT.Test.Unit.Validation
             _uut = new Vali(_airSpace);
         }
 
-        [TestCase("ATR423", "80000", "50000", "1000", "2018051563055")]
-        [TestCase("ATR423", "50000", "80000", "1000", "2018051563055")]
+        [TestCase("ATR423", "90000", "50000", "1000", "2018051563055")]
+        [TestCase("ATR423", "50000", "90000", "1000", "2018051563055")]
         [TestCase("ATR423", "50000", "50000", "500", "2018051563055")]
         [TestCase("ATR423", "50000", "50000", "20000", "2018051563055")]
-        [TestCase("ATR423", "0", "0", "15000", "2018051563055")]
+        [TestCase("ATR423", "10000", "10000", "15000", "2018051563055")]
         public void ValidateTrackTest_Passed(string tag, string xCoo, string yCoo, string aptitude, string timeStamp)
         {
             //ITrack track = new ATM_1.Track("ATR423", "39405", "12932", "14000", "2018051563055");
@@ -42,8 +42,8 @@ namespace AMT.Test.Unit.Validation
             Assert.That(result, Is.True);
         }
 
-        [TestCase("ATR423", "80001", "50000", "1000", "2018051563055")]
-        [TestCase("ATR423", "50000", "80001", "1000", "2018051563055")]
+        [TestCase("ATR423", "90001", "50000", "1000", "2018051563055")]
+        [TestCase("ATR423", "50000", "90001", "1000", "2018051563055")]
         [TestCase("ATR423", "50000", "50000", "499", "2018051563055")]
         [TestCase("ATR423", "50000", "50000", "499", "2018051563055")]
         [TestCase("ATR423", "50000", "50000", "20001", "2018051563055")]
